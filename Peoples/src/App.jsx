0adafe20594688f34062ff2a085a2cb2eaa2b43d
Ayden Xu <aydenxunc@gmail.com>
2024-02-18 11:58:23 -0500

    addedPeoples

---
+++ Peoples/src/App.jsx
@@ -0,0 +1,68 @@
+import './App.css'
+
+function App() {
+
+  return (
+<body>
+    <div className="grid-container">
+        <div className="grid-item1">1</div>
+        <div className="grid-item2">2</div>
+        <div className="grid-item3" id="SectionHead">[ Creative ]</div>
+        <div className="grid-item4">4</div>
+        <div className="grid-item5">5</div>
+        <div className="grid-item6">6</div>
+        <div className="grid-item7">7</div>
+        <div className="grid-item8">8</div>
+        <div className="grid-item9">9</div>
+        <div className="grid-item10">10</div>
+        <div className="grid-item11">11</div>
+        <div className="grid-item12" id="SectionHead">[ Photography ]</div>
+        <div className="grid-item13">13</div>
+        <div className="grid-item14">14</div>
+        <div className="grid-item15">15</div>
+        <div className="grid-item16">16</div>
+
+		<div className="hidden-div">
+			<div className="hidden-div-image">
+				<img src="https://i.ibb.co/vJnnZq3/Screenshot-2024-01-26-at-12-03-07-AM.png" alt="car"></img>
+			</div>
+			<div>
+                <h3>Matthew Akuamoah-Boateng</h3>
+				<p>Creative | Photo Member</p>
+			</div>
+		</div>
+		<div className="hidden-div2">
+			<div className="hidden-div-image2">
+				<img src="https://i.ibb.co/vJnnZq3/Screenshot-2024-01-26-at-12-03-07-AM.png" alt="car"></img>
+			</div>
+			<div>
+                <h3>Matthew Akuamoah-Boateng</h3>
+				<p>Creative | Photo Member</p>
+			</div>
+		</div>
+		<div className="hidden-div3">
+			<div className="hidden-div-image3">
+				<img src="https://i.ibb.co/vJnnZq3/Screenshot-2024-01-26-at-12-03-07-AM.png" alt="car"></img>
+			</div>
+			<div>
+                <h3>Matthew Akuamoah-Boateng</h3>
+				<p>Creative | Photo Member</p>
+			</div>
+		</div>
+		<div className="hidden-div4">
+			<div className="hidden-div-image4">
+				<img src="https://i.ibb.co/vJnnZq3/Screenshot-2024-01-26-at-12-03-07-AM.png" alt="car"></img>
+			</div>
+			<div>
+                <h3>Matthew Akuamoah-Boateng</h3>
+				<p>Creative | Photo Member</p>
+			</div>
+		</div>
+		
+
+    </div>
+</body>
+  )
+}
+
+export default App
