9a772df31197fc69c9e358ae20788626748de6ca
yonmaor1 <yonmaor@cmu.edu>
2024-03-10 18:03:33 -0400

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -5,34 +5,34 @@ import React, { useRef } from "react"
 import { Canvas, useFrame, useThree, extend } from "@react-three/fiber"
 import { createBrowserHistory } from 'history';
 
-const history = createBrowserHistory()
+export const history = createBrowserHistory()
 
-const fonts = {
+export const fonts = {
     Kumuna: "/fonts/Kommuna/Kommuna Demo 400 Narrow.otf",
     RobotoMono: "/fonts/RobotoMono/RobotoMono-VariableFont_wght.ttf",
     Wordmark: "/fonts/Wordmark/NewEdge666-Regular.otf",
     Wordmark_Rounded: "/fonts/Wordmark/NewEdge666-RegularRounded.otf",
 }
 
-const max = (a, b) => {
+export const max = (a, b) => {
     return a > b ? a : b
 }
 
-const min = (a, b) => {
+export const min = (a, b) => {
     return a < b ? a : b
 }
 
-const map = (val, ilo, ihi, olo, ohi) => { 
+export const map = (val, ilo, ihi, olo, ohi) => { 
     return olo + ((val - ilo) / (ihi - ilo)) * (ohi - olo)
 }
   
-const constrain = (val, lo, hi) => { 
+export const constrain = (val, lo, hi) => { 
     if (lo < val && val < hi) return val
     if (val <= lo) return lo
     if (val >= hi) return hi
 }
 
-const Cover = styled(a.div)`
+export const Cover = styled(a.div)`
   position: absolute;
   will-change: background, transform;
   top: 0;
@@ -41,7 +41,7 @@ const Cover = styled(a.div)`
   height: 100%;
 `
 
-function redirect(id) {
+export function redirect(id) {
     const pages = ["about", "tickets", "people", "lines"];
     console.log(pages[id])
 
@@ -53,11 +53,11 @@ function redirect(id) {
     return history.replace(location)
 }
   
-function sendBack() {
+export function sendBack() {
     return history.replace("/")
 }
 
-const Pane = ({ position, size, moveFunction, id }) => {
+export const Pane = ({ position, size, moveFunction, id }) => {
 
     const state = useThree()
     const ref = useRef()
@@ -80,4 +80,19 @@ const Pane = ({ position, size, moveFunction, id }) => {
     )
 }
 
-export { fonts, map, constrain, Cover, Pane, history, redirect, sendBack, min, max,}
+export const RobotoMono = ({ position, width, fontSize, text, ...props }) => {
+    return (
+        <Text 
+            fontSize={fontSize != null ? fontSize : 0.1}
+            position={position != null ? position : [0,0,0]}
+            font={fonts.RobotoMono}
+            characters="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!"
+            color={props.color == null ? "black" : props.color}
+            anchorX={props.anchorX == null ? "center" : props.anchorX}
+            anchorY={props.anchorY == null ? "middle" : props.anchorY}
+            maxWidth={width != null ? width : Infinity}
+            textAlign={props.textAlign == null ? "justify" : props.textAlign}
+            text={text}
+        />
+    )
+}
