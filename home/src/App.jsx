f9873409f9fcdc0d5535937f056bced7a3d9f489
yonmaor1 <yonmaor@cmu.edu>
2024-03-17 13:51:20 -0400

    home app.jsx

---
+++ home/src/App.jsx
@@ -0,0 +1,15 @@
+import { CameraControls } from '@react-three/drei'
+import './App.css'
+import blenderModel from './OLD-blenderModel.jsx'
+import Model from './Model.jsx'
+
+export default function App() {
+  
+  return (
+    <>
+    <CameraControls />
+      <ambientLight intensity={ 0.5 } />
+      <Model />
+    </>
+  )
+}
