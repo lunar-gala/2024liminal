bc82bf42002a803900521b3d137333fe104d80e2
yonmaor1 <yonmaor@cmu.edu>
2024-03-22 17:43:56 -0400

    added HomePage, Model

--- src/App.jsx
+++ src/App.jsx
@@ -12,8 +12,18 @@ import React,
 import { 
   Stats, 
   Loader, 
+  MeshTransmissionMaterial, 
+  RoundedBox, 
+  Text3D
 } from '@react-three/drei'
+import {
+  EffectComposer,
+  Bloom,
+  SelectiveBloom,
+} from "@react-three/postprocessing";
 import { Canvas, useFrame, useThree, extend } from "@react-three/fiber"
+import { useControls } from "leva";
+import * as THREE from "three";
 
 // nav
 import { useLocation, Switch, Route } from "wouter"
@@ -49,7 +59,7 @@ const App = () => {
   const [location] = useLocation()
 
   // transition duration
-  const duration = 2000;
+  const duration = 5000;
 
   const go_in = {
     from: { position: [0, 0, -10], rotation: [0, 0, 0], scale: [0, 0, 0], opacity: 0 },
@@ -73,8 +83,7 @@ const App = () => {
     <>
       <Cover>
         <Canvas camera={{ position: [0, 0, 20], fov: 50 }} gl={{ localClippingEnabled: true }} >
-          <color attach="background" args={["white"]} /> // bg
-
+          <color attach="background" args={(location == '/') ? [0, 0, 0] : [255, 255, 255]} /> // bg
           <ambientLight intensity={1}/>
           <directionalLight position={[0, 0, 5]} intensity={0.5} />
           <Suspense fallback={null}>
@@ -170,27 +179,182 @@ function PrivateRoute({ children, page, ...rest }) {
  * should go in seperate files
  */
 
-function HomePage() {
+function Model() {
+  const groupRef = useRef();
+  const { viewport } = useThree();
+  
+  const paneWidth = viewport.width * 0.5
+  const paneHeight = 0.3 * paneWidth
+  const textSize = 0.22 * paneHeight
+  const textRadScale = 0.9
+  const textY = 0.47 * paneWidth
 
-  const panes = []
-  for (let i=0; i<4; i++){
-    let position = [0, map(i, 0, 4, -6, 8), 0]
-    let size = [10, 0.05, 7]
-    let force = null // noise(state.clock.elapsedTime + position[0], 1)
+  useFrame(({ delta, pointer, clock }) => {
+    groupRef.current.rotation.x = clock.getElapsedTime() / 8;
+  });
+
+
+  function Pane({position, rotation, text, config, x, z, angle, id}) {
     
-    panes.push(<Pane position={position} size={size} moveFunction={null} key={i} id={i}/>)
+    return(
+      <>
+        <mesh 
+          position={position} 
+          rotation={rotation}
+          onClick = { (e) => sendBack() }
+          onPointerDown = { (e) => redirect(id) } 
+        >
+          <RoundedBox args={[paneHeight, paneWidth, 0.1]} radius={0.005} smoothness={2}>
+            <meshLambertMaterial {...lambertConfig}/>
+            {/* <MeshTransmissionMaterial
+              background={new THREE.Color("#ffffff")}
+              {...config}
+            /> */}
+          </RoundedBox>
+        </mesh>
+        
+        <Text3D
+          font="./fonts/Wordmark/NewEdge-666-Regular.json"
+          size={textSize}
+          height={0.01}
+          position={[-x*textRadScale, textY, -z*textRadScale]}
+          rotation={[0,-angle,-Math.PI / 2]}>
+          {text}
+          <meshBasicMaterial color="white" />
+        </Text3D>
+      </>
+    )
   }
 
+  const textConfig = {
+    positiontext: {value: [0.01, .06 * paneWidth, -0.4 * paneWidth]},
+    rotationtext: {value: [3.2 * paneWidth,3.14 * paneWidth, 0.07 * paneWidth]}
+  };
+
+  // Lower 'samples' and 'resolution' for better performance (less lag)
+  const config = {
+    meshPhysicalMaterial: false,
+    transmissionSampler: false,
+    backside: false,
+    samples: { value: 16, min: 1, max: 32, step: 1 },
+    resolution: { value: 1024, min: 256, max: 2048, step: 256 },
+    transmission: { value: .94, min: 0, max: 1 },
+    roughness: { value: 0.24, min: 0, max: 1, step: 0.01 },
+    thickness: { value: .4, min: 0, max: 10, step: 0.01 },
+    ior: { value: 1.28, min: 1, max: 5, step: 0.01 },
+    chromaticAberration: { value: 0.16, min: 0, max: 1 },
+    anisotropy: { value: 0.25, min: 0, max: 1, step: 0.01 },
+    anisotropicBlur: { value: 0.89, min: 0, max: 1, step: 0.01 },
+    distortion: { value: 0.23, min: 0, max: 1, step: 0.01 },
+    distortionScale: { value: 0.14, min: 0.01, max: 1, step: 0.01 },
+    temporalDistortion: { value: 0.19, min: 0, max: 1, step: 0.01 },
+    clearcoat: { value: 1.0, min: 0, max: 1 },
+    attenuationDistance: { value: 4.53, min: 0, max: 10, step: 0.01 },
+    attenuationColor: "#ffffff",
+    color: "#92969d",
+    bg: "#ffffff",
+  }
+
+  const lambertConfig = {
+    transparent: true,
+    opacity: 0.86,
+    depthTest: true,
+    depthWrite: true,
+    alphaTest: 0,
+    alphaHash: true,
+    visible: true,
+    side: THREE.DoubleSide,
+    color: '#5c5a5f',
+    emissive: '888888',
+    fog: true,
+    combine: THREE.MultiplyOperation,
+    reflectifity: 1,
+    refractionRatio: 1
+  }
+
+  function Scene() {
+    const { camera } = useThree();
+    camera.layers.enable(1); // Enable the bloom layer on the camera
+    camera.layers.enable(0); // Enable the bloom layer on the camera
+
+    const planes = [];
+    const radius = paneWidth / 3; // radius of the circle
+    const numPlanes = 12; // number of planes
+    var angle = (2 * Math.PI) / numPlanes;
+
+    // Generate the planes
+    const pages = ["about", "tickets", "people", "lines"];
+    for (let i = 0; i < numPlanes; i++) {
+      angle = ((2 * Math.PI) / numPlanes) * i; // angle for each plane
+      var x = radius * Math.cos(angle);
+      var z = radius * Math.sin(angle);
+
+      let text = pages[i%4].toUpperCase()
+
+      planes.push(
+        <Pane key={i} id={i%4} position={[x, 0, z]} rotation={[0, -angle, 0]} text={text} config={config} x={x} z={z} angle={angle} />
+      );
+    }
+
+    return (
+      <>
+        {planes}
+      </>
+    )
+  }
+
+  const glowPosition = [0, 0, 0.2];
+  
+  // const glowSize = useControls({
+  //   glowSizing: {value: [2, 0.85, 0.01], step: 0.01}
+  // })
+  // const paneSize = useControls({
+  //   paneSizing: {value: [0.45, 2, 0.01], step: 0.01}
+  // })
+
+
+  const { rotation } = useControls({
+    rotation: {
+      value: [0, 0, 1.57], // initial value
+      step: 0.01, // step size for each value
+    },
+  });
+
+  // Checkout 'Clone' props from R3F Drei docs: https://github.com/pmndrs/drei?tab=readme-ov-file#clone
   return (
     <>
-      <group position={[0, 0, 0]}>
-        {panes}
+      <group
+        ref={groupRef}
+        dispose={null}
+        position={[0, 0, 0]}
+        rotation={rotation}
+      >
+        <Scene />
       </group>
+
+      {/* glowing panel */}
+      <mesh {...glowPosition}>
+        <boxGeometry args={[paneWidth, 0.25 * paneWidth, 0.01]} />
+        <meshStandardMaterial
+          emissive="white"
+          color={"white"}
+          toneMapped={false}
+        />
+      </mesh>
+      {/* Bloom documentation : https://docs.pmnd.rs/react-postprocessing/effects/bloom */}
+      {/* <EffectComposer>
+        <Bloom luminanceThreshold={0} luminanceSmoothing={0.1} height={300} />
+      </EffectComposer> */}
     </>
-  )
-  
+  );
 }
 
 
-
-
+function HomePage(viewport) {
+  return (
+    <>
+      <fog attach="fog" color="#758ac1" near={1} far={10} />
+      <Model />
+    </>
+  )
+}
